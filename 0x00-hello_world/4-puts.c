8b676fe1f8e11283910cbf875403b2b1835c353d
Jocatemir <rasheedsaheed5658@gmai.com>
2022-09-10 20:53:16 -0700

    Learning betty style with alx project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-/** main - Entry point
+/**
+ * main - Entry point
+ *
  * Description: prints
+ *
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
