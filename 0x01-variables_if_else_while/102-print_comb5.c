7bae4794bcf538c7774c6cb1d89e44372275342b
Jocatemir <rasheedsaheed5658@gmai.com>
2022-09-12 14:06:50 -0700

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - printing numbers in 2's
+ *
+ * Return: 0 always
+ *
+ */
+
+int main(void)
+{
+	int j;
+	int z;
+
+	for (j = 0; j <= 98; j++)
+	{
+	for (z = 1; z <= 99; z++)
+	{
+	putchar((j / 10) + '0');
+	putchar((j % 10) + '0');
+	putchar(' ');
+	putchar((z / 10) + '0');
+	putchar((z % 10) + '0');
+	if (j == 98 && z == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
