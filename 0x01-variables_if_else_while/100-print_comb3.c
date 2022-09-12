e86f12eeb596dd8dfbd556fe95621e9eb1db5715
Jocatemir <rasheedsaheed5658@gmai.com>
2022-09-11 19:31:20 -0700

    Learning c programing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,19 @@
  */
 int main(void)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < 9; i++)
 	{
+	for (j = i = 1; j < 10; j++)
+	{
 	putchar((i % 10) + '0');
-	if (i == 9)
+	putchar((j % 10) + '0');
+	if (i == 8 && j == 9)
 	continue;
 	putchar(',');
 	putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
