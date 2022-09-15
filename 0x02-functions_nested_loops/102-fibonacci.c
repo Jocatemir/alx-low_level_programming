45e3c56477b3578838ccd2c3adbc3bed69e9ed79
Jocatemir <rasheedsaheed5658@gmai.com>
2022-09-15 14:01:04 -0700

    nested loop with Alx

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,6 +12,7 @@ int main(void)
 	long int f1 = 1;
 	long int f2 = 2;
 	int i;
+
 	printf("%li", f1);
 	for (i = 0; i < 49; i++)
 	{
@@ -20,5 +21,6 @@ int main(void)
 	f1 = f2;
 	f2 = new;
 	}
-	printf("\n");return (0);
+	printf("\n");
+	return (0);
 }
