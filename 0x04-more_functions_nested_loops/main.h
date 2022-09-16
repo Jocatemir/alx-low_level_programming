8ce7e4abb9962f38530739b5286c74db052e5c85
Jocatemir <rasheedsaheed5658@gmai.com>
2022-09-16 16:06:22 -0700

    Learning more about function

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,6 @@
-#ifndef _main_h_
+#ifndef MAIN_H
 
-#define _main_h_
+#define MAIN_H
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,8 +11,8 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
-void print_square(int size)
+void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif
+#endif /*function*/
