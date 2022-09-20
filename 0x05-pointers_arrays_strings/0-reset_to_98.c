a60cf0d86a4379ce1664034216e4f250bb307892
Jocatemir <rasheedsaheed5658@gmai.com>
2022-09-19 22:59:35 -0700

    function and arrary

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,19 +3,12 @@
 #include<stdio.h>
 
 /**
-
  * reset_to_98 - resseting a pointer to another number
-
  * @n: is the input
-
  * Return: n
-
  */
 
 void reset_to_98(int *n)
-  
 {
-  
-  *n = 98;
-  
+	*n = 97;
 }
