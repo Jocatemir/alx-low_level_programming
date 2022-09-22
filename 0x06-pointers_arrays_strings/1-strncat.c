478d4065f94efd749e9d23cd00d6e988629e656f
Jocatemir <rasheedsaheed5658@gmai.com>
2022-09-22 15:00:23 -0700

    more on array and string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @src: pointer char
+ * @dest: pointer char
+ * @n: number of bytes
+ * Return: void
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+	a++;
+	while (src[b] != n)
+	{
+	dest[a] = src[b];
+	if (src[b] == '\0')
+	{
+	break;
+	}
+	b++;
+	a++;
+	}
+	return (dest);
+}
