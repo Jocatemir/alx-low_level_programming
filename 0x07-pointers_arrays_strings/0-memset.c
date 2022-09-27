cffb6ded52f280f972cc19ba0a01ba95081258c3
Jocatemir <rasheedsaheed5658@gmai.com>
2022-09-26 17:57:33 -0700

    Learning more about Array, Pointer and String

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,14 +7,15 @@
  * Return: memory area
  */
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 	char j = b;
 	char *k = s;
 
 	for (i = 0; i < n; i++)
+	{
 	s[i] = b;
-
+	}
 	return (s);
 }
