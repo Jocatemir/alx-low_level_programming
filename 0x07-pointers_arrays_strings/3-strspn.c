35ac82c1fe632ffc84f53c31676528d8d3fc6f06
Jocatemir <rasheedsaheed5658@gmai.com>
2022-09-26 20:11:05 -0700

    Coding is fun

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,8 +3,8 @@
 /**
  * _strspn - function that get the length of a prefix
  * @accept: bytes source
- * @s: initial segment
- * Return: number
+ * @s: string
+ * Return: value
  */
 
 unsigned int _strspn(char *s, char *accept)
@@ -15,17 +15,17 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (a = 0; s[a] != '\0'; a++)
 	{
-		c = 0;
-		for (n = 0; accept[n] != '\0'; n++)
-		{
-			if (accept[n] == s[a])
-			{
-				v++;
-				c = 1;
-			}
-		}
+	c = 0;
+	for (n = 0; accept[n] != '\0'; n++)
+	{
+	if (accept[n] == s[a])
+	{
+	v++;
+	c = 1;
+	}
+	}
 	if (c == 1)
 	return (v);
 	}
-		return (v);
+	return (v);
 }
