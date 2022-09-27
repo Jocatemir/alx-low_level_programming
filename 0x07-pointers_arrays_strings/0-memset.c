f5b12a7ff468def29106dc55295b87ac2cfa3a96
Jocatemir <rasheedsaheed5658@gmai.com>
2022-09-26 17:17:54 -0700

    Learning more about Array, Pointer and String

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset -write function that fill memory of constant bytes
+ *
+ * Return: memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int n;
+
+	for (n = 0; n <= 4; n++)
+	{
+		*(s + 1) = b
+	}
+	return (s)
+}
