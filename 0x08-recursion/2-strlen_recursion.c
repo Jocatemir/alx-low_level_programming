bdc1b9b8fc5bcf4190b91a16fb052b1b2c60d30c
Jocatemir <rasheedsaheed5658@gmai.com>
2022-10-03 13:17:50 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: address operator.
- * Return: The length of the string.
-*/
+ * _strlen_recursion -  returns the lenght of the strings
+ * @s: this is the variable fortje string
+ * Return: nohing just zero
+ */
 
 int _strlen_recursion(char *s)
-{
-	int l = 0;
 
+{
 	if (*s != '\0')
 	{
-		l++;
-		_strlen_recursion(s + 1);
-	}
-	return(l);
+		return (1 + _strlen_recursion(s + 1));
+}
+	else
+	{
+	return (0);
+}
 }
